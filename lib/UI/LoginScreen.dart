c364fe4ec833ce968d08636cf42830264c1fdf49
Omkar Rajaram Gaikwad <omkar.gaikwad0308@gmail.com>
2020-12-28 00:14:24 +0530

    login ui complete

--- lib/UI/LoginScreen.dart
+++ lib/UI/LoginScreen.dart
@@ -16,12 +16,105 @@ class _LoginScreenState extends State<LoginScreen> {
       child: SafeArea(
           child: Scaffold(
               backgroundColor: Colors.white,
-              body: Container(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/AppLogo.jpg"),
-                    ]),
+              body: Center(
+                child: Container(
+                  margin: EdgeInsets.all(30),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/AppLogo.jpg",
+                          height: 150,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 45,
+                          child: TextFormField(
+                              autocorrect: false,
+                              controller: username,
+                              decoration: InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.person, color: Colors.black),
+                                  enabledBorder: const OutlineInputBorder(
+                                      // width: 0.0 produces a thin "hairline" border
+                                      borderSide: const BorderSide(
+                                          color: Colors.black, width: 0.9),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(15))),
+                                  focusedBorder: const OutlineInputBorder(
+                                    // width: 0.0 produces a thin "hairline" border
+                                    borderSide: const BorderSide(
+                                        color: Colors.black, width: 0.9),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                      borderSide: const BorderSide(
+                                          color: Colors.black)),
+                                  labelText: "Username *",
+                                  labelStyle:
+                                      TextStyle(color: Colors.black54))),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 45,
+                          child: TextFormField(
+                              autocorrect: false,
+                              controller: username,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.lock, color: Colors.black),
+                                  suffixIcon: Icon(
+                                    Icons.visibility,
+                                    color: Colors.black,
+                                  ),
+                                  enabledBorder: const OutlineInputBorder(
+                                      // width: 0.0 produces a thin "hairline" border
+                                      borderSide: const BorderSide(
+                                          color: Colors.black, width: 0.9),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(15))),
+                                  focusedBorder: const OutlineInputBorder(
+                                    // width: 0.0 produces a thin "hairline" border
+                                    borderSide: const BorderSide(
+                                        color: Colors.black, width: 0.9),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                      borderSide: const BorderSide(
+                                          color: Colors.black)),
+                                  labelText: "Password *",
+                                  labelStyle:
+                                      TextStyle(color: Colors.black54))),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        RaisedButton(
+                          padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          onPressed: () {},
+                          color: Colors.black,
+                          child: Text(
+                            "LOGIN",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                                letterSpacing: 2),
+                          ),
+                        )
+                      ]),
+                ),
               ))),
     );
   }
